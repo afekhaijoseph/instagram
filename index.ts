307d2805c64b903c998e0fbfdb23f401c1066bf2
afekhaijoseph <jobenzystein@gmail.com>
2020-01-15 21:39:00 +0100

    comment addition functionality

---
+++ index.ts
@@ -0,0 +1,10 @@
+var container=document.querySelector("#comment");
+var button=document.querySelector("button");
+var input=document.querySelector("input");
+var first='<div class="comment-list"><img  class="resize" src="dance.jpg" alt=""><div class="actual-comment"><p> <strong>picasso </strong>';
+var second='</p><div class="function"><p class="gray">4 days ago <strong> reply</strong></p>    </div><i class="far fa-heart"></i></div>';
+button.addEventListener("click", function(){
+    input.value;
+    container.innerHTML+=first + input.value + second;
+    input.value="";
+});
