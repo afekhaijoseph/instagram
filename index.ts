0766fa698cbdcbdd25a7b3bd1bc4bce70a08d59a
afekhaijoseph <jobenzystein@gmail.com>
2020-01-20 21:26:39 +0100

    reply functionality

--- index.ts
+++ index.ts
@@ -1,10 +1,34 @@
-var container=document.querySelector("#comment");
-var button=document.querySelector("button");
-var input=document.querySelector("input");
-var first='<div class="comment-list"><img  class="resize" src="dance.jpg" alt=""><div class="actual-comment"><p> <strong>picasso </strong>';
-var second='</p><div class="function"><p class="gray">4 days ago <strong> reply</strong></p>    </div><i class="far fa-heart"></i></div>';
-button.addEventListener("click", function(){
-    input.value;
-    container.innerHTML+=first + input.value + second;
-    input.value="";
-});
+var container = document.querySelector("#comment");
+var button = document.querySelector("button");
+var container2=document.querySelector("#your-comment");
+var input = container2.querySelector("input");
+var replies = document.getElementsByClassName("clap-back");
+var likes = document.getElementsByClassName("fa-heart");
+var num = 12;
+var first ='<div class="comment-list" ';
+var second='><img  class="resize" src="dance.jpg" alt=""><div class="actual-comment"><p> <strong>picasso </strong>';
+var third = '</p><div class="function"><p class="gray">4 days ago <strong> reply</strong></p>    </div><i class="far fa-heart"></i></div>';
+var fourth= '<div id="reply-content"><img src="glasses.jpeg" alt=""><div class="actual-comment"><p> <strong>pyro </strong>';
+var fifth = ' </p><div class="function"><p class="gray">1hr ago <strong class="reply"> reply</strong></p>    </div></div></div>';
+//commentId= 'id="comment' + num;
+     input.submit function postComment(commentId){
+    if(input.placeholder=="Add a reply"){
+        document.getElementById(commentId).outerHTML+=fourth + input.value + fifth;
+      }
+      else{
+        num++;
+        input.value;
+        container.innerHTML += first +commentId+'"'+second + input.value + third;
+      }
+      input.value="";
+    }
+for(var i=0; i < likes.length; i++){
+    likes[i].addEventListener("click", function(){
+           this.classList.toggle("liked");
+    })
+}
+for(var i=0; i < replies.length; i++){
+    replies[i].addEventListener("click", function(){
+           input.placeholder="Add a reply"
+    })
+}
