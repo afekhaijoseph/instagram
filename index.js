fa4dec4109924674e8c31b0580c355c102c158e9
afekhaijoseph <jobenzystein@gmail.com>
2020-01-24 09:30:32 +0100

    Reply Functionality 2

--- index.js
+++ index.js
@@ -4,23 +4,17 @@ var container2 = document.querySelector("#your-comment");
 var input = container2.querySelector("input");
 var replies = document.getElementsByClassName("clap-back");
 var likes = document.getElementsByClassName("fa-heart");
-var num = 12;
+var num = 11;
 var first = '<div class="comment-list" ';
 var second = '><img  class="resize" src="dance.jpg" alt=""><div class="actual-comment"><p> <strong>picasso </strong>';
 var third = '</p><div class="function"><p class="gray">4 days ago <strong> reply</strong></p>    </div><i class="far fa-heart"></i></div>';
 var fourth = '<div id="reply-content"><img src="glasses.jpeg" alt=""><div class="actual-comment"><p> <strong>pyro </strong>';
 var fifth = ' </p><div class="function"><p class="gray">1hr ago <strong class="reply"> reply</strong></p>    </div></div></div>';
-//commentId= 'id="comment' + num;
-input.submit;
 function postComment(commentId) {
-    if (input.placeholder == "Add a reply") {
-        document.getElementById(commentId).outerHTML += fourth + input.value + fifth;
-    }
-    else {
-        num++;
-        input.value;
-        container.innerHTML += first + commentId + '"' + second + input.value + third;
-    }
+    document.getElementById("commentId").outerHTML += fourth + input.value + fifth;
+    num++;
+    input.value;
+    container.innerHTML += first + '"' + second + input.value + third;
     input.value = "";
 }
 for (var i = 0; i < likes.length; i++) {
@@ -30,6 +24,7 @@ for (var i = 0; i < likes.length; i++) {
 }
 for (var i = 0; i < replies.length; i++) {
     replies[i].addEventListener("click", function () {
-        input.placeholder = "Add a reply";
+        input.getAttribute('placeholder');
+        input.setAttribute('placeholder', 'Add a reply');
     });
 }
--- index.ts
+++ index.ts
@@ -4,31 +4,28 @@ var container2=document.querySelector("#your-comment");
 var input = container2.querySelector("input");
 var replies = document.getElementsByClassName("clap-back");
 var likes = document.getElementsByClassName("fa-heart");
-var num = 12;
+var num = 11;
 var first ='<div class="comment-list" ';
 var second='><img  class="resize" src="dance.jpg" alt=""><div class="actual-comment"><p> <strong>picasso </strong>';
 var third = '</p><div class="function"><p class="gray">4 days ago <strong> reply</strong></p>    </div><i class="far fa-heart"></i></div>';
 var fourth= '<div id="reply-content"><img src="glasses.jpeg" alt=""><div class="actual-comment"><p> <strong>pyro </strong>';
 var fifth = ' </p><div class="function"><p class="gray">1hr ago <strong class="reply"> reply</strong></p>    </div></div></div>';
-//commentId= 'id="comment' + num;
-     input.submit function postComment(commentId){
-    if(input.placeholder=="Add a reply"){
-        document.getElementById(commentId).outerHTML+=fourth + input.value + fifth;
-      }
-      else{
+   
+     function postComment(commentId){
+        document.getElementById("commentId").outerHTML+=fourth + input.value + fifth;
         num++;
         input.value;
-        container.innerHTML += first +commentId+'"'+second + input.value + third;
-      }
-      input.value="";
+        container.innerHTML += first +'"'+second + input.value + third;
+        input.value="";
     }
-for(var i=0; i < likes.length; i++){
-    likes[i].addEventListener("click", function(){
-           this.classList.toggle("liked");
-    })
+    for(var i=0; i < likes.length; i++){
+        likes[i].addEventListener("click", function(){
+        this.classList.toggle("liked");
+       })
 }
-for(var i=0; i < replies.length; i++){
-    replies[i].addEventListener("click", function(){
-           input.placeholder="Add a reply"
-    })
+    for(var i=0; i < replies.length; i++){
+        replies[i].addEventListener("click", function(){
+        input.getAttribute('placeholder')
+        input.setAttribute('placeholder', 'Add a reply');
+    });
 }
