7d20c86f7fd8d17addbc0f3c3ac821cdb03cd6e0
afekhaijoseph <jobenzystein@gmail.com>
2020-01-29 15:28:29 +0100

    README file addition

---
+++ README.md
@@ -0,0 +1,35 @@
+## Instagram-clone
+
+This project is based off of the original instagram mobile webpage in an attempt to recreate 
+as much fumctionality as possible using basic front-end languages.
+
+
+### Table Of Content.
+1. [Prerequisite.](#prerequisite)
+2. [Installation.](#installation)
+3. [Usuage.](#usuage)
+4. [credits](#credits)
+
+
+## Prerequisite.
+The prerequisites for the completion and funcitoning of this project are listed below.
+* A prefered IDE
+* Html.
+* Css.
+* javascript.
+* typescript.
+## Installation.
+Typescript, a superset of javascript was used in this project for the *compile time error* features to avoid run time errors. Typescript compiles into javascript and therefore has to installed.
+* Install node.js source code for your platform https://nodejs.org/en/download/
+* Verify if installation is successful by typing **node -v** in the terminal
+* type the following code in the terminal to install typescript
+```
+     npm install -g typescript
+
+```
+## Usuage.
+This project is built based off the instagram model. It includes the homepage, the profile page, and the comments page.
+It was built solely on frontend languages listed above,comments can be added to the page and the comments can be replied.
+## Credits.
+* Afekhai Joseph
+* Mr Evans
--- index.ts
+++ index.ts
@@ -7,16 +7,21 @@ var likes = document.getElementsByClassName("fa-heart");
 var num = 11;
 var first ='<div class="comment-list" ';
 var second='><img  class="resize" src="dance.jpg" alt=""><div class="actual-comment"><p> <strong>picasso </strong>';
-var third = '</p><div class="function"><p class="gray">4 days ago <strong> reply</strong></p>    </div><i class="far fa-heart"></i></div>';
+var third = '</p><div class="function"><p class="gray">4 days ago <input type="submit" value="reply" class="clap-back"onclick="replyComment(commentId)"></p> </div> </div><i class="far fa-heart"></i></div></div>';
 var fourth= '<div id="reply-content"><img src="glasses.jpeg" alt=""><div class="actual-comment"><p> <strong>pyro </strong>';
-var fifth = ' </p><div class="function"><p class="gray">1hr ago <strong class="reply"> reply</strong></p>    </div></div></div>';
-   
-     function postComment(commentId){
-        document.getElementById("commentId").outerHTML+=fourth + input.value + fifth;
-        num++;
-        input.value;
-        container.innerHTML += first +'"'+second + input.value + third;
-        input.value="";
+var fifth = '</p><div class="function"><p class="gray">1hr ago <strong class="reply"> reply</strong></p></div></div></div>';
+var selectComment;
+       function postComment(){
+         if(input.getAttribute('placeholder')=="Add a reply"){
+            selectComment.outerHTML+=fourth + input.value + fifth;
+         }
+         else{
+            num++;
+            input.value;
+            container.innerHTML += first+'"'+"id=comment" + num+ '"' +second + input.value + third;
+         }
+         input.value="";
+         input.setAttribute("placeholder","Add a comment");
     }
     for(var i=0; i < likes.length; i++){
         likes[i].addEventListener("click", function(){
@@ -25,7 +30,16 @@ var fifth = ' </p><div class="function"><p class="gray">1hr ago <strong class="r
 }
     for(var i=0; i < replies.length; i++){
         replies[i].addEventListener("click", function(){
-        input.getAttribute('placeholder')
-        input.setAttribute('placeholder', 'Add a reply');
+        if (input.placeholder=="Add a comment"){
+            input.getAttribute('placeholder')
+            input.setAttribute('placeholder', 'Add a reply');
+        }
+       else{
+           input.getAttribute("placeholder");
+           input.setAttribute("placeholder", "Add a comment");
+       }
     });
-}
+}   
+   function replyComment(commentId){
+    selectComment=document.getElementById(commentId);
+   }
